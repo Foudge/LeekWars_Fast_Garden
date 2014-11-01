c979ba0407e819a28668849a73a1ebf53df25241
Foudge <olivier.fouge@free.fr>
2014-11-01 22:36:10 +0100

    Ajout d'une fenêtre de configuration
    Pour sauvegarder de manière permanente la valeur des paramètres, j'ai
    utilisé GM_getValue et GM_setValue.
    A cause de l'abandon de "@grant none" j'ai été obligé de trouver une
    alternative à l'override de la fonction submitForm et d'utiliser
    unsafeWindow partout où nécessaire.

--- LeekWars_Fast_Garden.user.js
+++ LeekWars_Fast_Garden.user.js
@@ -6,8 +6,10 @@
 // @include     http://leekwars.com/index.php?page=garden
 // @downloadURL https://github.com/Foudge/LeekWars_Fast_Garden/raw/dev/LeekWars_Fast_Garden.user.js
 // @updateURL   https://github.com/Foudge/LeekWars_Fast_Garden/raw/dev/LeekWars_Fast_Garden.user.js
-// @version     0.0.6
-// @grant       none
+// @version     0.0.7
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @require     http://code.jquery.com/jquery-2.1.1.min.js
 // ==/UserScript==
 
 
@@ -27,8 +29,38 @@ function decCount(id) {
   counter[0].lastChild.nodeValue = ' ' + (count-1);
 }
 
-/* Override submitForm function */
-window.submitForm = function(page, params){
+function replaceSubmitForm()
+{
+  console.log('replaceSubmitForm');
+  var $unsafe_garden = unsafeWindow.$("#garden-right");
+  var $garden = window.$("#garden-right");
+  // Click d'un adversaire
+  $unsafe_garden.find(".leek.enemy").unbind("click");
+  $garden.find(".leek.enemy").click(function() {
+    mySubmitForm("garden_update", [
+      ['leek_id', $(this).parent().attr('leek')],
+      ['enemy_id', $(this).attr('id')]
+    ]);
+  });
+  // Click d'un farmer
+  $unsafe_garden.find(".enemy.farmer").unbind("click");
+  $garden.find('.enemy.farmer').click(function() {
+    mySubmitForm("garden_update", [
+      ['target_farmer', $(this).attr('id')]
+    ]);
+  });
+  // Click d'une compo adverse
+  $unsafe_garden.find(".enemyCompo").unbind("click");
+  $garden.find('.enemyCompo').click(function() {
+    mySubmitForm("garden_update", [
+      ['my_team', $(this).parent().attr('compo')],
+      ['target_team', $(this).attr('id')]
+    ]);
+  });
+}
+
+// My submitForm function
+function mySubmitForm(page, params){
   //demande annulé si combat en cours ou potager en cours de rechargement
   if (loading == true) {
     console.log('Combat en cours...');
@@ -40,8 +72,8 @@ window.submitForm = function(page, params){
     realParams[params[p][0]] = params[p][1];
     console.log(params[p][0] + '=' + params[p][1]);
   }
-  realParams.token = __TOKEN;
-  var myId = realParams.leek_id || realParams.my_team || __FARMER_ID;
+  realParams.token = unsafeWindow.__TOKEN;
+  var myId = realParams.leek_id || realParams.my_team || unsafeWindow.__FARMER_ID;
   var targetId = realParams.enemy_id || realParams.target_team || realParams.target_farmer;
   var targetName = getName(targetId);
   var fightType = FightTypeEnum.UNDEFINED;
@@ -203,11 +235,13 @@ function addFightResult(fight, duration, talent, xp, habs)
   else if (fight.result == ResultEnum.DEFEAT) class_name += " defeat";
   else if (fight.result == ResultEnum.DRAW) class_name += " draw";
   else class_name += " generating";
-  var $fightDiv = $("div.fight-history").find("a[href='/report/" + fight.fightId + "']");
+  var $fightDiv = $("#fight-" + fight.fightId);
   if ($fightDiv[0] == undefined) {
-    $fightDiv = $("<div>", {class: class_name});
+    $fightDiv = $("<div>", {class: class_name, id: ("fight-"+fight.fightId)});
     var $fightImg = $("<img>", {src: "http://static.leekwars.com/image/fight_black.png"});
-    var $fightLink = $("<a>", {href: "/report/" + fight.fightId});
+    var fight_link = use_report_link ? ("/report/" + fight.fightId) : ("/fight/" + fight.fightId);
+    var target_link = force_new_tab ? "_blank" : "_self";
+    var $fightLink = $("<a>", {href: fight_link, target: target_link});
     var $enemyLink = $("<a>", {href: "/leek/" + fight.targetId, text: " " + fight.targetName });
     $fightLink.append($fightImg);
     $fightDiv.append($fightLink);
@@ -216,19 +250,20 @@ function addFightResult(fight, duration, talent, xp, habs)
     else if (fight.type == FightTypeEnum.FARMER) $("#farmers").append($fightDiv);
     else if (fight.type == FightTypeEnum.TEAM) $("div.enemies-compos[compo='" + fight.myId + "']").append($fightDiv);
   } else {
-    $fightDiv = $fightDiv.parent();
     $fightDiv.attr('class', class_name);
   }
-  if (fight.result == ResultEnum.UNDEFINED)
-    $fightDiv.prop('title', "En attente d'analyse du résultat...");
-  else if (fight.result == ResultEnum.GENERATING)
-    $fightDiv.prop('title', "Combat en cours de génération...");
-  else {
-    var fight_title = duration;
-    if (talent != undefined) fight_title += ("\nTalent :" + talent);
-    if (xp != undefined) fight_title += ("\nXP : " + xp);
-    if (habs != undefined) fight_title += ("\nHabs : " + habs);
-    $fightDiv.prop('title', fight_title);
+  if (show_tooltip) {
+    if (fight.result == ResultEnum.UNDEFINED)
+      $fightDiv.prop('title', "En attente d'analyse du résultat...");
+    else if (fight.result == ResultEnum.GENERATING)
+      $fightDiv.prop('title', "Combat en cours de génération...");
+    else {
+      var fight_title = duration;
+      if (talent != undefined) fight_title += ("\nTalent :" + talent);
+      if (xp != undefined) fight_title += ("\nXP : " + xp);
+      if (habs != undefined) fight_title += ("\nHabs : " + habs);
+      $fightDiv.prop('title', fight_title);
+    }
   }
 }
 
@@ -240,12 +275,89 @@ function checkFights()
   }
 }
 
+function addSetupButton()
+{
+  var potager = document.getElementById("page").children[0];
+  potager.style.display = "inline-block";
+  var $setupDiv = $("<div>", {class: "button", style: "width: 24px; height: 24px; margin-left: 10px;"});
+  var $setupImg = $("<img>", {src: "http://static.leekwars.com/image/engrenage_black.png", style: "width: 100%; height: 100%;"});
+  $setupDiv.append($setupImg);
+  $setupDiv.insertAfter(potager);
+  $setupDiv.click(function(){$("#popups").css("display", "table");});
+}
+
+function createSetupPopup()
+{
+  var $setupDiv = $("<div>", {id: "setup-popup", class: "popup", style: "width: 600px; display: inline-block; opacity: 1;"});
+  var content = "<div class='title'>Paramètres du potager</div>\
+  <div class='content'>\
+    <input id='setting-compact' type='checkbox'>\
+    <label for='setting-compact'>Utiliser le mode compact (adapté aux faibles résolutions)</label><br>\
+    <input id='setting-report-link' type='checkbox'>\
+    <label for='setting-report-link'>Lien renvoyant vers le rapport de combat plutôt que sa visualisation</label><br>\
+    <input id='setting-newtab-link' type='checkbox'>\
+    <label for='setting-newtab-link'>Forcer l'ouverture du combat dans un nouvel onglet</label><br>\
+    <input id='setting-tooltip' type='checkbox'>\
+    <label for='setting-tooltip'>Afficher le résumé du combat dans une infobulle</label><br>\
+    <input id='setting-fix-avatar' type='checkbox'>\
+    <label for='setting-fix-avatar'>Corriger le problème d'affichage de l'avatar par défaut</label>\
+  </div>\
+  <div class='actions' style='display: flex;'>\
+    <div id='setting-ok'>OK</div>\
+    <div id='setting-cancel'>Annuler</div>\
+	</div>"
+  $setupDiv.append(content);
+  if (compact_mode) $setupDiv.find("#setting-compact").prop('checked', true);
+  if (use_report_link) $setupDiv.find("#setting-report-link").prop('checked', true);
+  if (force_new_tab) $setupDiv.find("#setting-newtab-link").prop('checked', true);
+  if (show_tooltip) $setupDiv.find("#setting-tooltip").prop('checked', true);
+  if (fix_avatar) $setupDiv.find("#setting-fix-avatar").prop('checked', true);
+  $setupDiv.find("#setting-ok").click(function(){saveSettingValues();$("#popups").css("display", "none");});
+  $setupDiv.find("#setting-cancel").click(function(){$("#popups").css("display", "none");});
+  $("#popups").find("td").append($setupDiv);
+}
+
+function loadSettingValues()
+{
+  compact_mode = GM_getValue("compact_mode", false);
+  use_report_link = GM_getValue("use_report_link", true);
+  force_new_tab = GM_getValue("force_new_tab", true);
+  show_tooltip = GM_getValue("show_tooltip", true);
+  fix_avatar = GM_getValue("fix_avatar", false);
+}
+
+function saveSettingValues()
+{
+  $setupDiv = $("div#setup-popup");
+  if ($setupDiv.length != 0) {
+    compact_mode = $setupDiv.find("#setting-compact").prop('checked');
+    console.log("compact_mode=" + compact_mode);
+    use_report_link = $setupDiv.find("#setting-report-link").prop('checked');
+    console.log("use_report_link=" + use_report_link);
+    force_new_tab = $setupDiv.find("#setting-newtab-link").prop('checked');
+    console.log("force_new_tab=" + force_new_tab);
+    show_tooltip = $setupDiv.find("#setting-tooltip").prop('checked');
+    console.log("show_tooltip=" + show_tooltip);
+    fix_avatar = $setupDiv.find("#setting-fix-avatar").prop('checked');
+    console.log("fix_avatar=" + fix_avatar);
+    GM_setValue("compact_mode", compact_mode);
+    GM_setValue("use_report_link", use_report_link);
+    GM_setValue("force_new_tab", force_new_tab);
+    GM_setValue("show_tooltip", show_tooltip);
+    GM_setValue("fix_avatar", fix_avatar);
+  } else {
+    console.log("setup-popup not found!");
+  }  
+}
+
+
 var FightTypeEnum = { UNDEFINED:0, SOLO:1, FARMER:2, TEAM:3 };
 var ResultEnum = { UNDEFINED:0, GENERATING:1, DRAW:2, WIN:3, DEFEAT:4 };
 var fights = [];
 var myFirstLeekId = 0;
 var myTeamId = 0;
 var loading = false;
+var compact_mode, use_report_link, force_new_tab, show_tooltip, fix_avatar;
 
 //wait page loaded
 window.addEventListener('load', function () {
@@ -254,6 +366,10 @@ window.addEventListener('load', function () {
   footer.setAttribute('style', 'display: none;');
   document.getElementById('page').style.setProperty('height', window.innerHeight - 150 + 'px', null);
   document.getElementById('wrapper').setAttribute('style', 'max-width: 1100px');
+  //add setup button
+  addSetupButton();
+  loadSettingValues();
+  createSetupPopup();
   // récupération d'infos sur l'éleveur
   myFirstLeekId = $("div.leek.myleek").attr("id");
   console.log("myFirstLeekId=" + myFirstLeekId);
@@ -267,12 +383,16 @@ window.addEventListener('load', function () {
       break;
     }
   }
+  // remplace submitForm par mySubmitForm
+  replaceSubmitForm();
   // remplace les avatars manquant par celui par défaut
-  var images = document.getElementById('garden-right').getElementsByTagName('img');
-  for(var i=0; i<images.length; i++) {
-    if (images[i].src.indexOf("no_avatar.png") != -1) {
-      console.log("avatar=" + images[i].src);
-      images[i].src = "http://i2.wp.com/static.leekwars.com/image/no_avatar.png";
+  if (fix_avatar) {
+    var images = document.getElementById('garden-right').getElementsByTagName('img');
+    for(var i=0; i<images.length; i++) {
+      if (images[i].src.indexOf("no_avatar.png") != -1) {
+        console.log("avatar=" + images[i].src);
+        images[i].src = "http://i2.wp.com/static.leekwars.com/image/no_avatar.png";
+      }
     }
   }
   //lancement du timer de révifications des combats
